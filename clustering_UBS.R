0a7e51eef5f4d466544c764d6088f99be0ba7157
sandorlukacs-dev <76122519+sandorlukacs-dev@users.noreply.github.com>
2021-01-20 22:47:33 +0100

    Clustering added

--- clustering_UBS.R
+++ clustering_UBS.R
@@ -21,50 +21,49 @@ library(fpc)
 #######################
 
 # Reading in data
-data <- read.csv('Ready_for_csv_export.csv', sep = ';')
+data <- read.csv('data.csv', sep = ';')
 
 # Preparing data
-data_nafix = na_if(data, "n.a.")
-data_nafix = na_if(data_nafix, 0)
-data_naomit = na.omit(data_nafix)
+data = na_if(data, "n.a.")
+data = na.omit(data)
 
-rownames(data_naomit) = data_naomit$Name
+rownames(data) = data$Name
 
 # Naming columns
 
-colnum <- as.vector(colnames(data_naomit[3:18]))
+colnum <- as.vector(colnames(data[2:16]))
 
 # Converting to numeric
-data_naomit[colnum] <- lapply(data_naomit[colnum], function(x) as.numeric(as.character(x)))
+data[colnum] <- lapply(data[colnum], function(x) as.numeric(as.character(x)))
 
 #######################
 ####### RATIOS ########
 #######################
 
 # 1. Ratio: Gross Loans (LTA)
-data_naomit$LTA <- data_naomit$Loans / data_naomit$TotalAssetes
+data$LTA <- data$Loans / data$TotalAssetes
 # 2. Ratio: Trade (TTA)
-data_naomit$TTA <- (data_naomit$TotalAssetes - data_naomit$LiquidAssets - data_naomit$Loans - data_naomit$Intangibles) / data_naomit$TotalAssetes
+data$TTA <- (data$TotalAssetes - data$LiquidAssets - data$Loans - data$Intangibles) / data$TotalAssetes
 
 # 3. Ratio: Trading Book
 
 # 4. Ratio: Interbank Lending (ILTA)
-data_naomit$ILTA <- (data_naomit$LoansAdvances + data_naomit$AssetRepos)  / data_naomit$TotalAssetes
+data$ILTA <- (data$LoansAdvances + data$AssetRepos)  / data$TotalAssetes
 # 5. Ratio: Interbank Borrowing (IBTA)
-data_naomit$IBTA <- (data_naomit$BankDeposits + data_naomit$LiabilityRepos)  / data_naomit$TotalAssetes
+data$IBTA <- (data$BankDeposits + data$LiabilityRepos)  / data$TotalAssetes
 # 6. Ratio: Wholesale Debt (WDA)
-data_naomit$WDA <- (data_naomit$OtherDepositsShortBorrowings + data_naomit$LongFunding)  / data_naomit$TotalAssetes
+data$WDA <- (data$OtherDepositsShortBorrowings + data$LongFunding)  / data$TotalAssetes
 # 7. Ratio: Stable Funding (SFA)
-data_naomit$SFA <- (data_naomit$CustomerDeposits + data_naomit$LongFunding)  / data_naomit$TotalAssetes
+data$SFA <- (data$CustomerDeposits + data$LongFunding)  / data$TotalAssetes
 # 8. Ratio: Deposits (CDA)
-data_naomit$CDA <- data_naomit$CustomerDeposits / data_naomit$TotalAssetes
+data$CDA <- data$CustomerDeposits / data$TotalAssetes
 # 9. Ratio: Interest Income (IIO)
-data_naomit$IIO = data_naomit$NetInterestIncome/data_naomit$OperatingIncome
+data$IIO = data$NetInterestIncome/data$OperatingIncome
 
 
 #Create data subset
 selected =c("LTA", "IBTA", "TTA", "ILTA", "WDA", "SFA", "CDA", "IIO")
-data_clustering = select(data_naomit, all_of(selected))
+data_clustering = select(data, all_of(selected))
 
 # Creating correllation matrix
 matrix <- cor(scale(data_clustering))
@@ -87,7 +86,8 @@ for (i in 3:(length(selected))) {
   for (k in 1:ncol(combinations)) {
     
     
-    data_subset = select(data_naomit, combinations[,k])
+    data_subset = select(data, combinations[,k])
+    data_subset = na.omit(data_subset)
     cluster <- (agnes(data_subset, method="ward"))
     container[[counter]] <-  cluster
     ac = append(ac, cluster$ac)
