d673a21e4f1050100a8f246549ce46473a79a207
sandorlukacs-dev <76122519+sandorlukacs-dev@users.noreply.github.com>
2021-01-25 14:32:01 +0100

    Promising clusters

--- clustering_UBS.R
+++ clustering_UBS.R
@@ -16,23 +16,29 @@ library(tidyverse)
 library(caret)
 library(plyr)
 library(fpc)
+library(MASS)
+library(clusterSim)
 
 #######################
 ######## DATA #########
 #######################
 
 # Reading in data
-data <- read.csv('data.csv', sep = ';')
+data <- read.csv('data1.csv', sep = ';')
+selected =c("Name","LoansAdvances", "LiabilityRepos", "BankDeposits", "LongFunding", "CustomerDeposits", "LiquidAssets", "Loans", "Intangibles", "NetInterestIncome", "OperatingIncome", "Equity", "TotalAssetes")
+data = select(data, all_of(selected))
+
+
 
 # Preparing data
-data = na_if(data, "n.a.")
+data = na_if(data, "NA")
 data = na.omit(data)
 
 rownames(data) = data$Name
 
 # Naming columns
 
-colnum <- as.vector(colnames(data[2:16]))
+colnum <- as.vector(colnames(data[2:12]))
 
 # Converting to numeric
 data[colnum] <- lapply(data[colnum], function(x) as.numeric(as.character(x)))
@@ -45,15 +51,12 @@ data[colnum] <- lapply(data[colnum], function(x) as.numeric(as.character(x)))
 data$LTA <- data$Loans / data$TotalAssetes
 # 2. Ratio: Trade (TTA)
 data$TTA <- (data$TotalAssetes - data$LiquidAssets - data$Loans - data$Intangibles) / data$TotalAssetes
-
-# 3. Ratio: Trading Book
-
 # 4. Ratio: Interbank Lending (ILTA)
-data$ILTA <- (data$LoansAdvances + data$AssetRepos)  / data$TotalAssetes
+#data$ILTA <- (data$LoansAdvances + data$AssetRepos)  / data$TotalAssetes
 # 5. Ratio: Interbank Borrowing (IBTA)
 data$IBTA <- (data$BankDeposits + data$LiabilityRepos)  / data$TotalAssetes
 # 6. Ratio: Wholesale Debt (WDA)
-data$WDA <- (data$OtherDepositsShortBorrowings + data$LongFunding)  / data$TotalAssetes
+#data$WDA <- (data$OtherDepositsShortBorrowings + data$LongFunding)  / data$TotalAssetes
 # 7. Ratio: Stable Funding (SFA)
 data$SFA <- (data$CustomerDeposits + data$LongFunding)  / data$TotalAssetes
 # 8. Ratio: Deposits (CDA)
@@ -63,7 +66,7 @@ data$IIO = data$NetInterestIncome/data$OperatingIncome
 
 
 #Create data subset
-selected =c("LTA", "IBTA", "TTA", "ILTA", "WDA", "SFA", "CDA", "IIO")
+selected =c("LTA", "IBTA", "TTA", "SFA", "CDA", "IIO")
 data_clustering = select(data, all_of(selected))
 
 # Creating correllation matrix
@@ -71,7 +74,7 @@ matrix <- cor(scale(data_clustering))
 corrplot(corr=matrix, method="number", title="Corellation Matrix of Potential Proxies")
 
 #!!!SFA Should be excluded
-selected =c("LTA", "IBTA", "TTA", "ILTA", "WDA", "CDA", "IIO")
+selected =c("LTA", "IBTA", "TTA", "CDA", "IIO")
 data_clustering = select(data, all_of(selected))
 
 ###########################
@@ -84,7 +87,7 @@ data_clustering = select(data, all_of(selected))
 container = list()
 ac = c()
 counter = 1
-for (i in 3:(length(selected))) {
+for (i in 4:(length(selected))) {
   
   combinations = combn(selected,i)
   
@@ -102,21 +105,39 @@ for (i in 3:(length(selected))) {
         
 }
 
-# Get the number of best clustering based on AC scor
+# Get the number of best clustering based on AC score
 max_index = which(ac==max(ac))
 
 #Save best clustering
+#best_clustering <- (agnes(data_subset, method="ward"))
 best_clustering = container[[max_index]]
 used_ratios = colnames(best_clustering$data)
 
+res <- array(0,c(6-3+1,2))
+res[,1] <- 3:6
+clusters <- NULL
+
+#for (i in 3:6) {
+  
+#  cl <- pam(best_clustering$data,i)
+#  res[i-3+1,2] <- G1 <- index.G1(best_clustering$data,cl$cluster,centrotypes="centroids")
+#  clusters <- rbind(clusters, cl$cluster)
+  
+#}
+
+#print(paste("max G1 for",(3:6)[which.max(res[,2])],"clusters=",max(res[,2])))
+#print("clustering for max G1")
+#plot(res, type="p", pch=0, xlab="Number of clusters", ylab="G1", xaxt="n")
+#axis(1, c(3:6))
+
 #Create clusters
-clust <- cutree(best_clustering, k = 4)
+clust <- cutree(best_clustering, k = 3)
 
 #Plot clusters
 
 pltree(best_clustering, hang=-1, cex = 0.6)
 
-rect.hclust(best_clustering, k = 4, border = 2:10)
+rect.hclust(best_clustering, k = 3, border = 2:10)
 
 fviz_cluster(list(data = best_clustering$data, cluster = clust), labelsize =7)
 
@@ -124,7 +145,7 @@ fviz_cluster(list(data = best_clustering$data, cluster = clust), labelsize =7)
 
 results_clustering <- data.frame(
                                   Name=best_clustering$order.lab, 
-                                  Segment=cutree(best_clustering,4)[best_clustering$order]
+                                  Segment=cutree(best_clustering,3)[best_clustering$order]
                                   )
 data_clustering$Name <- rownames(data_clustering)
 data_clustering <- data_clustering[order(data_clustering$Name),]
@@ -144,43 +165,3 @@ Mean.Values <- aggregate(x=results_clustering, by=list(results_clustering$Segmen
 Mean.Values <- select(Mean.Values, all_of(used_ratios))
 heatmap(as.matrix(Mean.Values))
 
-## hclust, method 'complete'
-d <- dist(data_clustering, method = "euclidean")
-hc1 <- hclust(d, method = "ward.D2" )
-plot(hc1, cex = 0.6, hang = -1)
-
-
-## agnes, method comparison
-#Note that agnes(*, method="ward") corresponds to hclust(*, "ward.D2")
-hc2 <- agnes(data_clustering, method = "complete")
-hc2$ac
-m <- c( "average", "single", "complete", "ward")
-names(m) <- c( "average", "single", "complete", "ward")
-ac <- function(x) {
-  agnes(data_clustering, method = x)$ac
-}
-map_dbl(m, ac)  
-
-## agnes, method 'ward'
-hc3 <- agnes(data_clustering, method = "ward")
-pltree(hc3, cex = 0.6, hang = -1, main = "Dendrogram of agnes")
-rect.hclust(hc3, k = 4, border = 2:10)
-
-
-
-clust <- cutree(hc3, k = 5)
-fviz_cluster(list(data = data_clustering1, cluster = clust))
-
-hc4 <- agnes(data_clustering1, method = "ward")
-pltree(hc4, cex = 0.6, hang = -1, main = "Dendrogram of agnes")
-rect.hclust(hc4, k = 5, border = 2:10)
-
-## dendextend
-hc_single <- agnes(data, method = "single")
-hc_complete <- agnes(data, method = "complete")
-hc_ward <- agnes(data, method = "ward")
-# converting to dendogram objects as dendextend works with dendogram objects 
-hc_single <- as.dendrogram(hc_single)
-hc_complete <- as.dendrogram(hc_complete)
-hc_ward <- as.dendrogram(hc_ward)
-tanglegram(hc_ward,hc_complete)
